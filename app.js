3e1cbdb1f9aaacc05df9ed816e9af910e95d956a
brianFleming20 <brian88@outlook.com>
2020-11-30 18:25:40 +0000

    update

--- app.js
+++ app.js
@@ -47,6 +47,6 @@ app.get("/about", function(req, res){
 res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Server running on port 3000.");
   });
