f20590f29fd5c1b1db207f74c3e3d49407548d60
brianFleming20 <brian88@outlook.com>
2020-12-01 17:46:47 +0000

    update views

--- app.js
+++ app.js
@@ -14,28 +14,42 @@ app.use(bodyParser.urlencoded({
 }));
 
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const personnelSite = "personnel site.";
+const commerse = "e-Commerse site.";
+const appSite = "application support site.";
 
+app.get("/", function(req, res) {
 
-app.get("/home", function(req, res) {
-  res.render("home");
+  res.render("home",{
+    personnal:personnelSite,
+    commerse: commerse,
+    application: appSite
+  });
 });
 
-app.get("/contact", function(req, res){
-res.render("contact");
-});
 
 app.get("/quotepersonnal", function(req, res){
-res.render("quotepersonnal");
+res.render("quoteapp",{
+  content: personnelSite
+});
 
 });
 
 app.get("/ecommerse", function(req, res){
-  res.render("ecommerse");
+  res.render("quoteapp",{
+    content: commerse
+  });
   
   });
 
+  app.get("/maintain", function(req, res){
+    res.render("maintain");
+  });
+
   app.get("/quoteapp", function(req, res){
-    res.render("quoteapp");
+    res.render("quoteapp",{
+      content: appSite
+    });
     
     });
 
